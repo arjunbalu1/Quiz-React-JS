4b1fc949659e742325399bc3924deb1d099baac1
Arjun Balu <arjunbear2000@gmail.com>
2025-05-16 13:01:50 +0530

    finished

---
+++ src/components/quiz.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import Results from "./results";
+
+function Quiz() {
+    const questionBank = [
+        {
+            question: "What is the capital of France?",
+            options: ["Paris", "London", "Rome", "Madrid"],
+            answer: "Paris"
+        },
+        {
+            question: "What is the capital of India?",
+            options: ["Mumbai", "New Delhi", "Chennai", "Kolkata"],
+            answer: "New Delhi"
+        },
+        {
+            question: "What is the capital of USA?",
+            options: ["New York", "Los Angeles", "Chicago", "Washington, D.C."],
+            answer: "Washington, D.C."
+        }
+    ]
+
+    const initialAnswers = [null, null, null]
+
+    const [userAnswers, setUserAnswers] = useState(initialAnswers);
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const [isQuizFinished, setIsQuizFinished] = useState(false);
+
+    const selectedAnswer = userAnswers[currentQuestion];
+
+    function handleSelectOption(option) {
+        const newUserAnswers = [...userAnswers];
+        newUserAnswers[currentQuestion] = option;
+        setUserAnswers(newUserAnswers);
+    }
+    function goToNext() {
+        if(currentQuestion === questionBank.length - 1){
+            setIsQuizFinished(true);
+        } else {
+            setCurrentQuestion(currentQuestion + 1);
+        }
+    }
+    function goToPrev() {
+        if (currentQuestion > 0) {
+        setCurrentQuestion(currentQuestion - 1);
+    }
+}
+
+    function restartQuiz() {
+        setUserAnswers(initialAnswers);
+        setCurrentQuestion(0);
+        setIsQuizFinished(false);
+    }
+
+if (isQuizFinished){
+    return <Results userAnswers={userAnswers} questionBank={questionBank} restartQuiz={restartQuiz} />
+}
+
+  return (
+  <div>
+    <h2>Question {currentQuestion + 1}</h2>
+    <p className="question">{questionBank[currentQuestion].question}</p>
+    {questionBank[currentQuestion].options.map((option) => ( 
+        <button className={"option" + (selectedAnswer === option ? " selected" : "")} onClick={() => handleSelectOption(option)}>{option}</button>
+    ))} 
+
+
+    <div className="nav-buttons">
+        <button onClick={goToPrev} disabled={currentQuestion === 0}>
+            Previous
+            </button>
+        <button onClick={goToNext} disabled={!selectedAnswer}>
+            {currentQuestion === questionBank.length - 1 ? "Finish Quiz" : "Next"}
+            </button>
+    </div>
+  </div>
+  )
+}
+
+export default Quiz;
